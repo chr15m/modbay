ef1811827352676f080fc8844105f02a9b93aeb6
Chris McCormick <chris@mccormick.cx>
2022-02-13 21:29:44 +0800

    Parsing mod files and entering them.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import curses
-from sys import exit
+import re
+from sys import exit, argv
 from os import listdir
+from os.path import splitext
 from time import sleep
+from subprocess import check_output, STDOUT
 
 import socket
 
@@ -12,9 +15,14 @@ s.connect(("127.0.0.1", 1232))
 
 logfile = open("log", "a")
 
+modspath = None
+
 def log(*msg):
     logfile.write(" ".join([str(i) for i in msg]) + "\n")
     logfile.flush()
+
+def run(cmd):
+    return check_output(cmd, stderr=STDOUT, shell=True)
 
 import npyscreen
 
@@ -52,10 +60,33 @@ def loop_selected(loops, values):
     log(msg)
     s.send(msg.encode("utf8"))
 
+def get_info(mod):
+    info = run("xmp --load-only -C " + modspath + "/" + mod).decode("utf8")
+    channels = re.findall("Channels\ +: (\d+)", info)[0]
+    comments = "\n".join(re.findall("> (.*?)[\n$]", info))
+    return [channels, comments]
+
+def exitform(F):
+    F.editing = False
+
+def makemodform(info, mod):
+    F = MyForm(name=mod, minimum_columns=20, minimum_lines=20)
+    #app.F = F
+    quit = F.add(npyscreen.ButtonPress, name = "back", when_pressed_function = lambda: exitform(F))
+    F.edit()
+
+def start_mod(mods, mod, F):
+    # extract the notes
+    info = get_info(mod)
+    log("Loading mod:", info)
+    #F.editing = False
+    makemodform(info, mod)
+    #F.DISPLAY()
+
 def makeform(app, selected=0):
         # These lines create the form and populate it with widgets.
         # A fairly complex screen in only 8 or so lines of code - a line for each control.
-        F = MyForm(name = "cf-fakeboy", minimum_columns=20, minimum_lines=20)
+        F = MyForm(name = "fakeboy", minimum_columns=20, minimum_lines=20)
         app.F = F
 
         #F.add_handlers({curses.ascii.ESC: self.exit_application})
@@ -75,20 +106,27 @@ def makeform(app, selected=0):
         #dt = F.add(npyscreen.TitleDateCombo, name = "Date:")
         #ml = F.add(npyscreen.MultiLineEdit, value="", max_height=5)
 
-        loops = [l for l in listdir("loops") if l.endswith(".wav")]
+        F.add(npyscreen.TitleText, name = "Mods:")
 
-        ms = F.add(npyscreen.TitleSelectOne, max_height=8, value = [0,], name="Loop", values = loops, scroll_exit=True)
-        ms.when_value_edited=lambda: loop_selected(loops, ms.value)
+        mods = []
+        mods = [m for m in listdir("mods") if splitext(m)[1] in [".it", ".xm", ".mod"]]
 
-        s  = F.add(npyscreen.TitleSlider, out_of=4, name = "Volume", value=2)
-        s.when_value_edited=lambda: send("volume " + str(s.value))
+        ms = F.add(npyscreen.MultiLineAction, max_height=8, value = [], name="Mod", values = mods, scroll_exit=True)
+        #ms.when_value_edited=lambda: start_mod(mods, ms.value, F)
+        ms.actionHighlighted=lambda item,key: start_mod(mods, item, F)
 
-        s2  = F.add(npyscreen.TitleSlider, out_of=4, name = "Ticks", value=1)
-        s2.when_value_edited=lambda: send("divider " + str(pow(2, s2.value)))
+        #s  = F.add(npyscreen.TitleSlider, out_of=4, name = "Volume", value=2)
+        #s.when_value_edited=lambda: send("volume " + str(s.value))
 
-        ms1 = F.add(npyscreen.TitleMultiSelect, max_height =-2, value = [], name="Stick", values=["<-"], scroll_exit=True)
-        ms1.when_value_edited=lambda: send("sticky " + str(0 in ms1.value and 1 or 0))
+        #s2  = F.add(npyscreen.TitleSlider, out_of=4, name = "Ticks", value=1)
+        #s2.when_value_edited=lambda: send("divider " + str(pow(2, s2.value)))
+
+        #ms1 = F.add(npyscreen.TitleMultiSelect, max_height =-2, value = [], name="Stick", values=["<-"], scroll_exit=True)
+        #ms1.when_value_edited=lambda: send("sticky " + str(0 in ms1.value and 1 or 0))
         
+        # skip a line
+        #F.nextrely += 1
+
         quit = F.add(npyscreen.ButtonPress, name = "quit", when_pressed_function = lambda: app.exit_application())
 
         #ms2= F.add(npyscreen.TitleMultiSelect, max_height =-2, value = [1,], name="Pick Several",
@@ -103,6 +141,7 @@ def makeform(app, selected=0):
 class TestApp(npyscreen.NPSApp):
     def main(self):
         makeform(self)
+        log("after make form")
         #print(ms.get_selected_objects())
 
     def exit_application(self):
@@ -115,6 +154,10 @@ class TestApp(npyscreen.NPSApp):
         exit()
 
 if __name__ == "__main__":
-    App = TestApp()
-    App.run()
+    if len(argv) != 2:
+        print("Usage: main.py " + "path-to-mods")
+    else:
+        modspath = argv[1]
+        App = TestApp()
+        App.run()
 
