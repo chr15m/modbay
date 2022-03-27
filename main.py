e6ad1a7c0e3ab4c03846e8d4466539845caf2f09
Chris McCormick <chris@mccormick.cx>
2022-03-27 20:15:31 +0800

    Cleaning up.

--- main.py
+++ main.py
@@ -38,10 +38,6 @@ class MyForm(npyscreen.FormBaseNew):
         log("adjust", x)
 
 def rebuild(F, app):
-    #F._clear_all_widgets()
-    #F.add(npyscreen.TitleText, name = "Changed:",)
-    #F.add(npyscreen.TitleFilename, name = "Filename:")
-    #F.DISPLAY()
     F.editing = False
     makeform(app)
 
@@ -85,7 +81,6 @@ def makesender(el, c, key, conv):
 
 def makemodform(info, mod):
     F = MyForm(name=mod, minimum_columns=20, minimum_lines=20)
-    #app.F = F
     play = F.add(npyscreen.Checkbox, value=False, name="play")
     play.whenToggled = lambda: send("play " + str(play.value and 1 or 0))
 
@@ -94,12 +89,10 @@ def makemodform(info, mod):
     F.nextrely += 1
 
     for c in range(info[0]):
-        on = F.add(npyscreen.Checkbox, value=True, name="ch " + str(c))
+        on = F.add(npyscreen.Checkbox, value=True, name="ch " + str(c), max_width=15)
         on.whenToggled = makesender(on, c, "volume", lambda v: str(v and 4 or 0))
-    F.nextrely += 1
-
-    for c in range(info[0]):
-        pan = F.add(npyscreen.Checkbox, value=True, name="left " + str(c))
+        F.nextrely += -1
+        pan = F.add(npyscreen.Checkbox, value=True, name="left " + str(c), relx=20, max_width=20)
         pan.whenToggled = makesender(pan, c, "pan", lambda v: str(v and 1 or 0))
     F.nextrely += 1
 
@@ -124,29 +117,8 @@ def start_mod(mods, mod, F):
     makemodform(info, mod)
 
 def makeform(app, selected=0):
-        # These lines create the form and populate it with widgets.
-        # A fairly complex screen in only 8 or so lines of code - a line for each control.
         F = MyForm(name = "fakeboy", minimum_columns=20, minimum_lines=20)
         app.F = F
-
-        #F.add_handlers({curses.ascii.ESC: self.exit_application})
-        #F.add_handlers({curses.ascii.TAB: self.exit_application})
-        #F.add_handlers({"   ": self.exit_application})
-        #F.add_handlers({27: self.exit_application})
-        #F.add_handlers({"KEY_F(2)": self.exit_application})
-        #F.add_handlers({"^Y": self.exit_application})
-        #F.add_handlers({88: lambda: log("pressed X")})
-        #F.add_handlers({89: lambda: log("pressed Y")})
-        #log(F.handlers)
-        #F.how_exited_handers[npyscreen.wgwidget.EXITED_ESCAPE]  = self.exit_application
-
-        #b  = F.add(npyscreen.ButtonPress, name = "COOL", when_pressed_function = lambda: app.exit_application())
-
-        #t  = F.add(npyscreen.TitleText, name = "Text:",)
-        #fn = F.add(npyscreen.TitleFilename, name = "Filename:")
-        #fn2 = F.add(npyscreen.TitleFilenameCombo, name="Filename2:")
-        #dt = F.add(npyscreen.TitleDateCombo, name = "Date:")
-        #ml = F.add(npyscreen.MultiLineEdit, value="", max_height=5)
 
         F.add(npyscreen.FixedText, value="Mods:", editable=False)
 
@@ -154,30 +126,10 @@ def makeform(app, selected=0):
         mods = [m for m in listdir("mods") if splitext(m)[1] in [".it", ".xm", ".mod"]]
 
         ms = F.add(npyscreen.MultiLineAction, max_height=8, value = [], name="Mod", values = mods, scroll_exit=True)
-        #ms.when_value_edited=lambda: start_mod(mods, ms.value, F)
         ms.actionHighlighted=lambda item,key: start_mod(mods, item, F)
-
-        #s  = F.add(npyscreen.TitleSlider, out_of=4, name = "Volume", value=2)
-        #s.when_value_edited=lambda: send("volume " + str(s.value))
-
-        #s2  = F.add(npyscreen.TitleSlider, out_of=4, name = "Ticks", value=1)
-        #s2.when_value_edited=lambda: send("divider " + str(pow(2, s2.value)))
-
-        #ms1 = F.add(npyscreen.TitleMultiSelect, max_height =-2, value = [], name="Stick", values=["<-"], scroll_exit=True)
-        #ms1.when_value_edited=lambda: send("sticky " + str(0 in ms1.value and 1 or 0))
-        
-        # skip a line
-        #F.nextrely += 1
 
         quit = F.add(npyscreen.ButtonPress, name = "quit", when_pressed_function = lambda: app.exit_application(), rely=-3, relx=-12)
 
-        #ms2= F.add(npyscreen.TitleMultiSelect, max_height =-2, value = [1,], name="Pick Several",
-        #        values = ["Option1","Option2","Option3"], scroll_exit=True)
-
-        #F.add_handlers({"^Y": lambda x: rebuild(F, app)})
-        #F.add_complex_handlers([[lambda k: test_func(k, ml), callback]])
-
-        # This lets the user interact with the Form.
         F.edit()
 
 class TestApp(npyscreen.NPSApp):
