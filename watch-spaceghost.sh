b416525a6b61effb89cb3488c6f3dd797ffb2623
Chris McCormick <chris@mccormick.cx>
2023-05-12 11:17:39 +0100

    Fix local UDP idiosyncracies.

--- watch-spaceghost.sh
+++ watch-spaceghost.sh
@@ -2,12 +2,12 @@
 
 sendit () {
   echo "Sending $1 for Spaceghost presence."
-  echo "$1 ;" > /dev/udp/localhost/33333
+  echo "$1;" > /dev/udp/127.0.0.1/33333
   echo "Done."
 }
 
 while [ 1 ]
 do
   ping -w 1 -c 1 spaceghost.local >/dev/null 2>/dev/null && sendit "1" || sendit "0"
-  sleep 1
+  sleep 5
 done
