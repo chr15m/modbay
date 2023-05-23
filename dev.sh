26b0c4564314aefd6098cbbe1a9f0a7f1080ced4
Chris McCormick <chris@mccormick.cx>
2023-05-23 21:30:42 +0800

    Stop Pd from stealing focus.

--- dev.sh
+++ dev.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 tail -f log &
+( for i in `seq 5`; do wmctrl -x -a xterm.Xterm; sleep 1; done ) &
 xterm -fa "Courier Prime" -fs 14 -geometry 53x20+550+50 -e 'eval "$(direnv export bash)"; ./main.sh --gui; read done'
 pkill -P $$
