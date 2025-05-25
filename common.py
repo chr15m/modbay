dd89c6ff1f584b6a86abf9ef1e2f1692703378df
Chris McCormick <chris@mccormick.cx>
2025-05-25 17:54:52 +0800

    New popup/notify class.

--- common.py
+++ common.py
@@ -18,6 +18,26 @@ class MyForm(npyscreen.FormBaseNew):
     def adjust_widgets(x):
         log("adjust", x)
 
+# notify code is from npyscreen original but tweaked:
+# https://github.com/npcole/npyscreen/blob/master/npyscreen/utilNotify.py#L30
+
+class Popup(npyscreen.fmForm.Form):
+    DEFAULT_LINES      = 12
+    DEFAULT_COLUMNS    = 40
+    SHOW_ATX           = 6
+    SHOW_ATY           = 3
+
+def notify(message, title="Message", form_color='STANDOUT', wrap=True):
+    message = npyscreen.utilNotify._prepare_message(message)
+    F   = Popup(name=title, color=form_color)
+    F.preserve_selected_widget = True
+    mlw = F.add(npyscreen.wgmultiline.Pager,)
+    mlw_width = mlw.width-1
+    if wrap:
+        message = npyscreen.utilNotify._wrap_message_lines(message, mlw_width)
+    mlw.values = message
+    F.display()
+
 def test_func(k, ml):
     #log("key test_func", args)
     ml.value += " " + str(k)
