3d57347bc91d67ef69bf85af9b6039cbdda6ad04
Chris McCormick <chris@mccormick.cx>
2025-07-05 16:58:53 +0800

    Udpate launcher script.

--- retropie-launcher.sh
+++ retropie-launcher.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+
+# Get the directory where the script is located
+APP_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 scriptdir=/home/pi/RetroPie-Setup
 source "$scriptdir/scriptmodules/system.sh"
 source "$scriptdir/scriptmodules/helpers.sh"
 source "$scriptdir/scriptmodules/inifuncs.sh"
 #source "$scriptdir/scriptmodules/packages.sh"
 joy2keyStart kcub1 kcuf1 kcuu1 kcud1 0x20 0x0a 0x58 0x59
-echo "Running $1";
-cd /home/pi/modbay
-./main.sh > /home/pi/modbay_run.log 2>&1
+cd "${APP_DIR}"
+date > modbay_run.log
+echo "Running modbay from ${APP_DIR}" >> modbay_run.log
+./main.sh >> modbay_run.log 2>&1
 joy2keyStop
 echo "Boot script exit"
 sleep 10
