f7fca5ff2f96edbcb2efbdd4165cf2f0b72c6f13
Chris McCormick <chris@mccormick.cx>
2025-07-05 17:25:21 +0800

    Move log to start.

--- retropie-launcher.sh
+++ retropie-launcher.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-
 # Get the directory where the script is located
 APP_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd "${APP_DIR}"
+date > modbay_run.log
+echo "Running modbay from ${APP_DIR}" >> modbay_run.log
 
 scriptdir=/home/pi/RetroPie-Setup
 source "$scriptdir/scriptmodules/system.sh"
@@ -9,9 +12,6 @@ source "$scriptdir/scriptmodules/helpers.sh"
 source "$scriptdir/scriptmodules/inifuncs.sh"
 #source "$scriptdir/scriptmodules/packages.sh"
 joy2keyStart kcub1 kcuf1 kcuu1 kcud1 0x20 0x0a 0x58 0x59
-cd "${APP_DIR}"
-date > modbay_run.log
-echo "Running modbay from ${APP_DIR}" >> modbay_run.log
 ./main.sh >> modbay_run.log 2>&1
 joy2keyStop
 echo "Boot script exit"
